0cefa8e12a6c1658a5b29f81b1cf601e4ad1f9b7
Marcel <marcel_endres@yahoo.de>
2020-09-24 00:03:54 +0200

    wrap up

--- Sudokujl/app/resources/field/FieldController.jl
+++ Sudokujl/app/resources/field/FieldController.jl
@@ -5,6 +5,20 @@ module FieldController
   
   function drawField()
     failed, table = Sudoku.solve()
+    html(:field, :drawField, table = table)
+  end
+
+  function clearField()
+    table = zeros(Int8, 9, 9)
+    html(:field, :drawField, table = table)
+  end
+
+  function solve()
+    failed, table = Sudoku.solve()
     html(:field, :drawField, table = table, failed = failed)
+  end
+
+  function next()
+    html("coming soon")
   end
 end
